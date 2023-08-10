5a81c3e0c03cf8faa7be605cc55557df84dcb4b6
ayush-anilan <109529899+ayush-anilan@users.noreply.github.com>
2023-08-10 13:18:08 +0530

    Final

---
+++ script.js
@@ -0,0 +1,44 @@
+const apiKey = "4d337d5311b94690bbe164121230508";
+const apiUrl =
+  "http://api.weatherapi.com/v1/current.json?key=4d337d5311b94690bbe164121230508&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 400) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.location.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.current.temp_c) + "°C";
+    document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.current.wind_kph + " km/h";
+
+    if (data.current.condition.text == "Partly cloudy") {
+      weatherIcon.src = "/images/clouds.png";
+    } else if (
+      data.current.condition.text == "Sunny" ||
+      data.condition.text == "Clear"
+    ) {
+      weatherIcon.src = "/images/clear.png";
+    } else if ((data.current.condition.text = "Mist")) {
+      weatherIcon.src = "/images/Mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
